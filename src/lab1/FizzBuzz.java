e58ebb970421d1cba0e312d1285d1f0bf1d8a705
Lucas Duan <130019403+dlucas1025@users.noreply.github.com>
2026-09-15 14:40:10 -0400

    Added some shit

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -16,7 +16,7 @@ class FizzBuzz {
         }
     }
 
-    private static void doFizzBuzz(int count) {
+    public static void doFizzBuzz(int count) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = count % 3 == 0;
         boolean divisibleBy5 = count % 5 == 0;
